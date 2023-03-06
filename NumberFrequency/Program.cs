58fea980122326dc27e058538960f2a72225b82f
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2023-03-06 23:56:00 +1100

    Add project files.

---
+++ NumberFrequency/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberFrequency
+{
+    internal class Program
+    {
+        //Write a program in C# Sharp to display the number and frequency of number from giving array.
+        //https://www.w3resource.com/csharp-exercises/linq/index.php
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter at least 10 different integers: ");
+            NumberSet.numbers
+
+        }
+
+        public void NumberSet()
+        {
+            int numbers;
+        }
+    }
+
+    public class NumberFrequency
+    {
+
+        public 
+
+    }
+}
